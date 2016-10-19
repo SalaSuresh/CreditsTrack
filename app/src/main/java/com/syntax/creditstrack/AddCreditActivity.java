f4ec4787f2df575f1e09320870607710c30b062e
Suresh <suresh.sala1992@gmail.com>
2016-10-19 14:26:21 +0530

    long back commit

--- app/src/main/java/com/syntax/creditstrack/AddCreditActivity.java
+++ app/src/main/java/com/syntax/creditstrack/AddCreditActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -14,13 +17,17 @@ import java.util.List;
  */
 public class AddCreditActivity extends AppCompatActivity {
     AutoCompleteTextView textNames;
+    RadioButton radioButton_They, radioButton_I;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_add_credit);
         DatabaseHandler db = new DatabaseHandler(this);
-        textNames=(AutoCompleteTextView)findViewById(R.id.autoCompleteTextView_Name);
+        textNames=(AutoCompleteTextView) findViewById(R.id.autoCompleteTextView_Name);
+        radioButton_They = (RadioButton) findViewById(R.id.radioButton_They);
+        radioButton_I = (RadioButton) findViewById(R.id.radioButton_I);
 
+        //TextView Name
         List<Credits> distinctCreditNames = db.getDistinctCreditNames();
         String names[] = new String[distinctCreditNames.size()];
         int i = 0;
@@ -33,5 +40,30 @@ public class AddCreditActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,names);
         textNames.setAdapter(adapter);
         textNames.setThreshold(1);
+
+        //Radio Button They
+        radioButton_They.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                checkOweStatus();
+                radioButton_I.setChecked(false);
+            }
+        });
+
+        //Radio Button I
+        radioButton_I.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                checkOweStatus();
+                radioButton_They.setChecked(false);
+            }
+        });
+    }
+
+    public void checkOweStatus(){
+        boolean status_They = radioButton_They.isChecked();
+        boolean status_I = radioButton_I.isChecked();
+        Toast.makeText(AddCreditActivity.this, "THEY: "+status_They+"\n I: "+status_I, Toast.LENGTH_SHORT).show();
     }
 }
